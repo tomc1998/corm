4908d5b28b999f12e9ab00f3117c5b2180fac89e
Thomas Cheng <thomascheng1998@googlemail.com>
2018-03-21 23:06:11 +0000

    Added SQL statement creation for defentity

--- main/main.lisp
+++ main/main.lisp
@@ -1,9 +1,39 @@
+(defun kebab-to-snake-case (name)
+  "Convert the string from a string separated with '-' to a string separated with '_'"
+  (reduce (lambda (&optional s0 s1) (concatenate 'string (string s0) (string s1)))
+          (loop for c across name collect (if (char= c #\-) #\_ (char-downcase c)))))
+
+(defun snake-to-kebab-case (name)
+  "Convert the string from a string separated with '_' to a string separated with '-'"
+  (reduce (lambda (&optional s0 s1) (concatenate 'string (string s0) (string s1)))
+          (loop for c across name collect (if (char= c #\_) #\- (char-downcase c)))))
+
+(defun sql-mod-from-keyword (s)
+  "Converts a SQL mod keyword to a valid string sql modifier. For example,
+  :primary maps to \"PRIMARY KEY\". Passing a string in as s simply makes this a
+  passthrough function, and returns the input."
+  (cond
+    ((typep s 'string) s)
+    ((eq s :primary) "PRIMARY KEY")
+    ((eq s :default) "DEFAULT")
+    ((eq s :not-null) "NOT NULL")
+    ((eq s :auto-increment) "AUTO_INCREMENT")))
+
+(defun slot-to-column-def (s)
+  "Given a list like (id \"BIGINT UNSIGNED\" :primary :auto-increment), turns it
+  into a string like \"id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT\"."
+
+  (concatenate 'string
+               (kebab-to-snake-case (string (nth 0 s))) " " ; Name
+               (string (nth 1 s)) " " ; Type
+               ;; Reduce whatever remains to a string
+               (reduce (lambda (&optional s0 s1) (concatenate 'string s0 " " s1))
+                       (loop for m in (cdr (cdr s)) collect (sql-mod-from-keyword m)))))
 
 (defmacro defentity (name slots)
   "Define an entity with the given name. This macro creates a class with the
   entity of that name, and creates the appropriate corresponding persistence
   storage. You can create a new entity with the make-<name> function.
-
 
   Slots can be given to type the data for storage in a database. Each slot is a
   list, the first item being the name of the slot, the second being the type in
@@ -14,18 +44,27 @@
 
   ;; Define a user entity
   (defentity user
-      ((id \"BIGINT UNSIGNED\" 'primary 'auto-increment)
-      (first-name \"VARCHAR (256)\" 'not-null)
-      (last-name \"VARCHAR (256)\" 'not-null)
+      ((id \"BIGINT UNSIGNED\" :primary :auto-increment)
+      (first-name \"VARCHAR (256)\" :not-null)
+      (last-name \"VARCHAR (256)\" :not-null)
       ))"
 
   ;; First, loop & extract out the column definitions from the slots, and also
   ;; the slot names.
-  (let ((column-defs (loop for s in slots collect s))
+  (let ((sql-def
+         (concatenate
+          'string
+          "CREATE TABLE IF NOT EXISTS "
+          (kebab-to-snake-case (string name))
+          " ("
+          (reduce (lambda (s0 s1) (format nil"~a,~%~a" s0 s1))
+                  (loop for s in slots collect (slot-to-column-def s)))
+          ");"))
         (slot-names (loop for s in slots collect
                          `(,(car s) :initarg
                             ,(intern (string (car s)) :keyword)))))
+    (print sql-def)
     `(defclass
-       ,name () ,slot-names)
+           ,name () ,slot-names)
     ))
 
--- test/main.lisp
+++ test/main.lisp
@@ -18,4 +18,25 @@
             "Slot 'id should be changed when using setf")
   )
 
+;; Test sql to lisp names
+(prove:is (kebab-to-snake-case "my-lisp-name") "my_lisp_name"
+          "Kebab to snake case conversion should work")
+(prove:is (kebab-to-snake-case "my_lisp_name") "my_lisp_name"
+          "Kebab to snake case conversion should work")
+(prove:is (snake-to-kebab-case "my_lisp_name") "my-lisp-name"
+          "Snake to kebab case conversion should work")
+(prove:is (snake-to-kebab-case "my-lisp-name") "my-lisp-name"
+          "Snake to kebab case conversion should work")
+
+;; Test keywords to SQL mods
+(prove:is (sql-mod-from-keyword :primary) "PRIMARY KEY")
+(prove:is (sql-mod-from-keyword :default) "DEFAULT")
+(prove:is (sql-mod-from-keyword :not-null) "NOT NULL")
+(prove:is (sql-mod-from-keyword :auto-increment) "AUTO_INCREMENT")
+(prove:is (sql-mod-from-keyword "Custom Value") "Custom Value")
+
+;; Test creating column defs
+(prove:is (slot-to-column-def '(id "BIGINT UNSIGNED" :primary :auto-increment))
+          "id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT")
+
 (prove:finalize)
