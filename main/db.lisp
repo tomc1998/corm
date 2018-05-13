b4638c7d48a5117133026aef0b88e793bb0425ba
Thomas Cheng <thomascheng1998@googlemail.com>
2018-05-13 17:45:55 +0100

    Made connections robust to mysql server disconnects

--- main/db.lisp
+++ main/db.lisp
@@ -4,19 +4,30 @@
 (defvar *pool*)
 (defvar *curr-conn-ix*)
 
+(defun connect-to-db ()
+  (dbi:connect :mysql :database-name "test"
+               :username "root" :password ""))
+
 (defun create-pool (size)
-  (setf *pool* (loop for i from 0 to size collect
-                    (dbi:connect :mysql :database-name "test"
-                                 :username "root" :password "")))
+  (setf *pool* (loop for i from 0 to size collect (connect-to-db)))
   (setf *curr-conn-ix* 0))
 
 (create-pool 10)
 
+(defun advance-conn-ix ()
+  "Advance *curr-conn-ix* circularly (when we hit the end, loop back to 0)"
+  (setf *curr-conn-ix* (1+ *curr-conn-ix*))
+  (if (>= *curr-conn-ix* (length *pool*))
+      (setf *curr-conn-ix* 0))
+  )
+
 (defun get-conn ()
   (let ((conn (nth *curr-conn-ix* *pool*)))
-    (setf *curr-conn-ix* (1+ *curr-conn-ix*))
-    (if (>= *curr-conn-ix* (length *pool*))
-        (setf *curr-conn-ix* 0))
+    (loop while (not (dbi:ping conn)) do
+         (setf (nth *curr-conn-ix* *pool*) (connect-to-db))
+         (advance-conn-ix)
+         (setf conn (nth *curr-conn-ix* *pool*)))
+    (advance-conn-ix)
     conn))
 
 ;; Some utils for names
@@ -24,9 +35,11 @@
   "Given an entity and a slot name, convert it to a value ready to be put into
 params"
   (let ((v (slot-value e slot)))
-    (if (is-slot-bool e slot) (if v 1 0)
-        v
-        )))
+    (cond
+      ((is-slot-bool e slot) (if v 1 0))
+      ((numberp v) (format nil "~$" v))
+      (t v))
+    ))
 
 (defun kebab-to-snake-case (name)
   "Convert the string from a string separated with '-' to a string separated with '_'"
